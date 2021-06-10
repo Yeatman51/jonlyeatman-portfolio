38931db722822634b583b11210227b71614311f8
Jon Yeatman <jonlyeatman@gmail.com>
2021-06-10 18:31:01 -0400

    New components set up

---
+++ src/components/Header/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <div className="yeatman-logo"></div>
+
+      <h1 className="header-text">
+      Full Stack Web Developer<br></br>
+      Graphic Designer
+      </h1>
+    </header>
+  
+  );
+}
+
+export default Header;
