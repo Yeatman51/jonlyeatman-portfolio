8f55abe111c0e0e8287bf445ae98d56e69d2d6a3
jon <jonlyeatman@gmail.com>
2021-06-28 06:28:12 -0400

    Designer page images And grid layout

--- src/pages/Designer.js
+++ src/pages/Designer.js
@@ -8,7 +8,7 @@ function Designer() {
         <h2 className="designer-text-header">Designer</h2>
 
         <p className="designer-text-paragraph">
-
+          ;llujhfg skaldjhg 
         </p>
       </div>
 
@@ -16,9 +16,69 @@ function Designer() {
         
       <div className="gd-gallery">
 
-        <div className="gd-gallery-img">
-          <img src={process.env.PUBLIC_URL + "/images/yeatman-logo.png"} alt="test"/>
+        <div>
+          <img className="gd-gallery-img" src={process.env.PUBLIC_URL + "/images/designs-img/Yeatman_GD_2.png"} alt="test"/>
         </div>
+
+        <div>
+          <img className="gd-gallery-img" src={process.env.PUBLIC_URL + "/images/designs-img/MiPonderosaV2_Final.png"} alt=""/>
+        </div>
+
+        <div>
+            <img className="gd-gallery-img" src={process.env.PUBLIC_URL + "/images/designs-img/Yeatman_GD_3.png"} alt=""/>
+        </div>
+
+        <div>
+            <img className="gd-gallery-img" src={process.env.PUBLIC_URL + "/images/designs-img/Yeatman_GD_4.png"} alt=""/>
+        </div>
+
+        <div>
+            <img className="gd-gallery-img" src={process.env.PUBLIC_URL + "/images/designs-img/Yeatman_GD_11.png"} alt=""/>
+        </div>
+
+        <div>
+            <img className="gd-gallery-img" src={process.env.PUBLIC_URL + "/images/designs-img/Yeatman_GD_5.png"} alt=""/>
+        </div>
+
+        <div>
+            <img className="gd-gallery-img" src={process.env.PUBLIC_URL + "/images/designs-img/Yeatman_GD_7.png"} alt=""/>
+        </div>
+        
+        <div>
+            <img className="gd-gallery-img" src={process.env.PUBLIC_URL + "/images/designs-img/Yeatman_GD_10.png"} alt=""/>
+        </div>
+
+        <div>
+            <img className="gd-gallery-img" src={process.env.PUBLIC_URL + "/images/designs-img/Yeatman_GD_9.png"} alt=""/>
+        </div>
+
+        <div>
+            <img className="gd-gallery-img" src={process.env.PUBLIC_URL + "/images/designs-img/Yeatman_GD_6.png"} alt=""/>
+        </div>
+
+        <div>
+            <img className="gd-gallery-img" src={process.env.PUBLIC_URL + "/images/designs-img/Yeatman_GD_20.png"} alt=""/>
+        </div>   
+
+        <div>
+            <img className="gd-gallery-img" src={process.env.PUBLIC_URL + "/images/designs-img/Webpage phoenix custom printing.png"} alt=""/>
+        </div>
+
+        <div>
+            <img className="gd-gallery-img" src={process.env.PUBLIC_URL + "/images/designs-img/Yeatman_GD_17.png"} alt=""/>
+        </div>
+
+        <div>
+            <img className="gd-gallery-img" src={process.env.PUBLIC_URL + "/images/designs-img/Yeatman_GD_21.png"} alt=""/>
+        </div>
+
+        <div>
+            <img className="gd-gallery-img" src={process.env.PUBLIC_URL + "/images/designs-img/Yeatman_GD_19.png"} alt=""/>
+        </div>
+
+        <div>
+            <img className="gd-gallery-img" src={process.env.PUBLIC_URL + "/images/designs-img/Yeatman_GD_16.png"} alt=""/>
+        </div> 
 
       </div>
 
