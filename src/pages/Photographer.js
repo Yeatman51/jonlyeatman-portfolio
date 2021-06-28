678bd1796aaaa25f91c06d5264dfa410f6df7494
jon <jonlyeatman@gmail.com>
2021-06-28 10:51:30 -0400

    Added images to photography page

--- src/pages/Photographer.js
+++ src/pages/Photographer.js
@@ -13,6 +13,69 @@ function Photographer() {
 
       <div className="dividers-line"></div>
 
+      <div class="photography-gallery">
+
+        <div>
+            <img className="photography-gallery-img" src={process.env.PUBLIC_URL + "/images/photos-img/Yeatman_1.jpg"} alt=""/>
+        </div>
+
+        <div>
+            <img className="photography-gallery-img" src={process.env.PUBLIC_URL + "/images/photos-img/Yeatman_2.jpg"} alt=""/>
+        </div>
+
+        <div>
+            <img className="photography-gallery-img" src={process.env.PUBLIC_URL + "/images/photos-img/Yeatman_3.jpg"} alt=""/>
+        </div>
+
+        <div>
+            <img className="photography-gallery-img" src={process.env.PUBLIC_URL + "/images/photos-img/Yeatman_4.jpg"} alt=""/>
+        </div>
+
+        <div>
+            <img className="photography-gallery-img" src={process.env.PUBLIC_URL + "/images/photos-img/Yeatman_5.jpg"} alt=""/>
+        </div>
+
+        <div>
+            <img className="photography-gallery-img" src={process.env.PUBLIC_URL + "/images/photos-img/Yeatman_6.jpg"} alt=""/>
+        </div>
+
+        <div>
+            <img className="photography-gallery-img" src={process.env.PUBLIC_URL + "/images/photos-img/Yeatman_7.jpg"} alt=""/>
+        </div>
+
+        <div>
+            <img className="photography-gallery-img" src={process.env.PUBLIC_URL + "/images/photos-img/Yeatman_8.jpg"} alt=""/>
+        </div>
+
+        <div>
+            <img className="photography-gallery-img" src={process.env.PUBLIC_URL + "/images/photos-img/Yeatman_9.jpg"} alt=""/>
+        </div>
+
+        <div>
+            <img className="photography-gallery-img" src={process.env.PUBLIC_URL + "/images/photos-img/Yeatman_10.jpg"} alt=""/>
+        </div>
+
+        <div>
+            <img className="photography-gallery-img" src={process.env.PUBLIC_URL + "/images/photos-img/Yeatman_11.jpg"} alt=""/>
+        </div>
+
+        <div>
+            <img className="photography-gallery-img" src={process.env.PUBLIC_URL + "/images/photos-img/Yeatman_12.jpg"} alt=""/>
+        </div>
+
+        <div>
+            <img className="photography-gallery-img" src={process.env.PUBLIC_URL + "/images/photos-img/Yeatman_13.jpg"} alt=""/>
+        </div>
+
+        <div>
+            <img className="photography-gallery-img" src={process.env.PUBLIC_URL + "/images/photos-img/Yeatman_14.jpg"} alt=""/>
+        </div>
+
+        <div>
+            <img className="photography-gallery-img" src={process.env.PUBLIC_URL + "/images/photos-img/Yeatman_15.jpg"} alt=""/>
+        </div>
+      </div>
+
     </div>
   
   );
