3f325ca672bd433f8613e631040aa0087181b93f
jon <jonlyeatman@gmail.com>
2021-06-28 04:39:25 -0400

    Developer card portfolio update with wall pro

--- src/pages/Developer.js
+++ src/pages/Developer.js
@@ -7,6 +7,15 @@ function Developer() {
   const cards = [
     {
       key: 1,
+      title:"Wall Pro",
+      imgSrc:"/images/dev-card-img/wall-pro.png",
+      alt:"pic",
+      description:"This business focused application allows project managers to quickly enter Show room measurements and convert them into necessary panels for production",
+      github:"https://github.com/mbussert/shaw-ppc",
+      www:"http://www.WallPro.net",
+    },
+    {
+      key: 2,
       title:"Treat YO Shelf",
       imgSrc:"/images/dev-card-img/treat-search.png",
       alt:"pic",
@@ -15,37 +24,37 @@ function Developer() {
       www:"https://treatyourshelf.herokuapp.com/",
     },
     {
-      key: 2,
+      key: 3,
       title:"DINGEN",
       imgSrc:"/images/dev-card-img/dingen-home.png",
       alt:"pic",
       description:"DINGEN is a food app design to take the struggle of preparing tonight's dinner",
       github:"https://github.com/Yeatman51/DinGen-Web",
       www:"https://yeatman51.github.io/DinGen-Web/",
-    },
-    {
-      key: 3,
-      title:"Coding Quiz",
-      imgSrc:"/images/dev-card-img/coding-quiz.png",
-      alt:"pic",
-      description:"This is a coding quiz designed for Developers in training to review course material",
-      github:"https://github.com/Yeatman51/coding-quiz",
-      www:"https://yeatman51.github.io/coding-quiz/",
     }
   ]
   
   const cardsRow2 = [
     {
       key: 4,
-      title:"Company Password Generator",
-      imgSrc:"/images/dev-card-img/password-generator.png",
+      title:"Coding Quiz",
+      imgSrc:"/images/dev-card-img/coding-quiz.png",
       alt:"pic",
-      description:"This webpage is designed to be a quick reference guide to help front in web developers",
-      github:"https://github.com/Yeatman51/company-password-generator",
-      www:"https://yeatman51.github.io/company-password-generator/",
+      description:"This is a coding quiz designed for Developers in training to review course material",
+      github:"https://github.com/Yeatman51/coding-quiz",
+      www:"https://yeatman51.github.io/coding-quiz/",
     },
+    // {
+    //   key: 5,
+    //   title:"Company Password Generator",
+    //   imgSrc:"/images/dev-card-img/password-generator.png",
+    //   alt:"pic",
+    //   description:"This webpage is designed to be a quick reference guide to help front in web developers",
+    //   github:"https://github.com/Yeatman51/company-password-generator",
+    //   www:"https://yeatman51.github.io/company-password-generator/",
+    // },
     {
-      key: 5,
+      key: 6,
       title:"Todo App",
       imgSrc:"/images/dev-card-img/todo-list.png",
       alt:"pic",
@@ -54,7 +63,7 @@ function Developer() {
       www:"https://yeatman51.github.io/todo-app/",
     },
     {
-      key: 6,
+      key: 7,
       title:"Web Cheat Sheet",
       imgSrc:"/images/dev-card-img/web-cheatsheet.png",
       alt:"pic",
